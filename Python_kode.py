7914b1149feaed38a845a3d79afd21a762402f38
Andreas Larsen <akla18@student.aau.dk>
2018-10-26 10:40:01 +0200

    Initial Commit

--- Python_kode.py
+++ Python_kode.py
@@ -1,3 +1,14 @@
-#Authors: Akla og Maja
+import numpy as np
 
-print(1+1)
+planes = 200
+lanes = 1
+t = 0
+
+landing = np.random.uniform(0,200,planes)
+arrival = np.random.uniform(0,200,planes)
+
+for i in len(arrival):
+    if t >= arrival[i]:
+        t += arrival[i]
+
+print(t)
