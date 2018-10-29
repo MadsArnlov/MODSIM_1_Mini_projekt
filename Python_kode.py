2bdb13f1fa3b2166683a62e9eecba42a7d25b3e4
Andreas <larsenandreas98@gmail.com>
2018-10-29 18:56:54 +0100

    Functions & Docstrings
    Converted the code blocks to functions and inserted basic docstrings for each function

--- Python_kode.py
+++ Python_kode.py
@@ -1,91 +1,129 @@
 import numpy as np
 
-planes = 200
-lanes = 2
 
-# =============================================================================
-# Creates arrival times with the distribution from file 'ankomsttider.dat'.
-# =============================================================================
-
-arrivals = np.loadtxt('interarrival.dat', dtype = int, delimiter = ',')
-
-arrival = np.array([])
-
-for line in arrivals:
-
-    x = line[0]
-    y = line[1] + 1
-
-    if line[2] == 0:
-        prob = 0
-    else:
-        prob = int(planes // (arrivals.sum(axis=0)[2] / line[2]))
-    if prob == 0:
-        interval = [0]
-    else:
-        interval = np.random.randint(x, y, prob)
-
-    arrival = np.concatenate((arrival, interval), axis = None)
-
-np.random.shuffle(arrival)
-
-# =============================================================================
-# Creates landing times with the distribution from file 'landingstider.dat'.
-# =============================================================================
-
-landings = np.loadtxt('duration.dat', dtype = int, delimiter = ',')
-
-landing = np.array([])
-
-for line in landings:
-
-    x = line[0]
-    y = line[1] + 1
-
-    if line[2] == 0:
-        prob = 0
-    else:
-        prob = int(planes // (landings.sum(axis=0)[2] / line[2]))
-    if prob == 0:
-        interval = [0]
-    else:
-        interval = np.random.randint(x, y, prob)
-
-    landing = np.concatenate((landing, interval), axis = None)
-
-np.random.shuffle(landing)
-
-# =============================================================================
-# Computes total and average wait time and the wait time for each plane.
-# =============================================================================
-
-queWait = [0 for i in range(lanes)]
-laneWait = []
-
-for i in range(len(arrival)):
-        queWait += -arrival[i]
+def arrivalTimes(planes):
+    
+    """
+    arrivalTimes(planes)
+    
+    Returns arrival times for planes with the distribution from file 'interarrival.dat'
+    
+    Parameters
+    ----------
+    planes : int
+        Number of planes trying to land
         
-        for j in range(len(queWait)):
+    Returns
+    -------
+    out : ndarray
+        One dimentional array filled with arrival times of planes
+    """
+    
+    arrivals = np.loadtxt('interarrival.dat', dtype = int, delimiter = ',')
+    
+    arrival = np.array([])
+    
+    for line in arrivals:
+    
+        x = line[0]
+        y = line[1] + 1
+    
+        if line[2] == 0:
+            prob = 0
+        else:
+            prob = int(planes // (arrivals.sum(axis=0)[2] / line[2]))
+        if prob == 0:
+            interval = [0]
+        else:
+            interval = np.random.randint(x, y, prob)
+    
+        arrival = np.concatenate((arrival, interval), axis = None)
+    
+    np.random.shuffle(arrival)
+    return(arrival)
+
+
+def landingTimes(planes):
+    
+    """
+    landingTimes(planes)
+    
+    Returns landing times for planes with the distribution from file 'duration.dat' as an array
+    
+    Parameters
+    ----------
+    planes : int
+        Number of planes trying to land
+        
+    Returns
+    -------
+    out : ndarray
+        One dimentional array filled with landing times of planes
+    """
+    
+    landings = np.loadtxt('duration.dat', dtype = int, delimiter = ',')
+    
+    landing = np.array([])
+    
+    for line in landings:
+    
+        x = line[0]
+        y = line[1] + 1
+    
+        if line[2] == 0:
+            prob = 0
+        else:
+            prob = int(planes // (landings.sum(axis=0)[2] / line[2]))
+        if prob == 0:
+            interval = [0]
+        else:
+            interval = np.random.randint(x, y, prob)
+    
+        landing = np.concatenate((landing, interval), axis = None)
+    
+    np.random.shuffle(landing)
+    return(landing)
+
+
+def waitTime(lanes, arrivalTimes, landingTimes):
+    
+    """
+    waitTime(lanes, arrivalTimes, landingTimes)
+    
+    Prints the average and total wait time and returns waittimes as ndarray
+    
+    Parameters
+    ----------
+    lanes : int
+        Number of lanes at the airfield
+    arrivalTimes : ndarray
+        Array containing the arrival times of the planes
+    landingTimes : ndarray
+        Array containing the landing times of the planes
+    """
+    
+    queWait = [0 for i in range(lanes)]
+    laneWait = []
+    
+    for i in range(len(arrivalTimes)):
+            queWait += -arrivalTimes[i]
             
-            if queWait[j] < 0:    
-                queWait[j] = 0
+            for j in range(len(queWait)):
                 
-        laneWait.append(min(queWait)) 
-        queWait[queWait.argmin()] += landing[i]
-
-print(sum(laneWait))
+                if queWait[j] < 0:    
+                    queWait[j] = 0
+                    
+            laneWait.append(min(queWait)) 
+            queWait[queWait.argmin()] += landingTimes[i]
+    
+    print("Total wait time:", sum(laneWait), "seconds")
+    print("Average wait time:", sum(laneWait)/len(laneWait), "seconds")
+    
+    return(laneWait)
         
-        
-wait = [0]
+arrival = arrivalTimes(200)
+landing = landingTimes(200)
 
-for i in range(1, len(arrival)):
-    W = (landing[i-1] - arrival[i]) + wait[i-1]
-
-    if W < 0:
-        W = 0
-
-    wait.append(W)
-
-average_wait_time = sum(wait)/len(wait)
-total_wait_time = sum(wait)
-print(total_wait_time, '\n', average_wait_time)
+print("Length of array 'arrival':", len(arrival))
+print("Length of array 'landing':", len(landing))    
+    
